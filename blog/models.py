8e4bf6c0a9a074407c854bd5cd79519c6944136c
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-04-25 16:29:04 +0100

    Add excerpt and updated_on fields to blog

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,6 @@ class Post(models.Model):
                                on_delete=models.CASCADE, related_name='blog_posts')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
