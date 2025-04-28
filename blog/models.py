fca775f8185e091dfd8521f3ecb88c660e21ec5d
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-04-28 14:40:18 +0100

    Add metaclass to Posts Model

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        # ordering = ['-created_on']
+        ordering = ['-created_on', 'author']
+    def __str__(self):
+        # return self.title
+        return f"{self.title} | written by {self.author} | created_on: {self.created_on}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -24,4 +30,5 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
     
